8aa91d9088dae8b6925a4aec8157af980be28e4a
Matt Whalen <mawhal@gmail.com>
2018-10-19 13:44:34 -0700

    code to merge CVENT data with session info

--- 2018/R code/merge_sessions_abstracts.r
+++ 2018/R code/merge_sessions_abstracts.r
@@ -6,8 +6,11 @@ library( tidyverse )
 
 
 # read data
-session <- read.csv( "../Data Files/SessionNumbers.csv", stringsAsFactors = FALSE )
-astract <- read.csv( "../Data Files/Report2.csv", stringsAsFactors = FALSE )
+session  <- read.csv( "../Data Files/SessionNumbers.csv", stringsAsFactors = FALSE )
+abstract <- read.csv( "../Data Files/Report2.csv", stringsAsFactors = FALSE )
 
 # merge sessions INTO abstract
+d <- left_join( abstract, session )
 
+# write to disk
+write.csv( d, "../Data Files/WSN2018_abstracts_sessions.csv" , row.names=FALSE )
